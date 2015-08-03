3ecf8ac21b4ac60ce61836dd15f99a6fadb0d3e3
Kristen Rieger <karieger1@gmail.com>
2015-08-03 01:41:13 -0500

    finally i can go on with my life

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,50 +1,41 @@
 $(document).on("ready", start);
 
-var inputBox = $("inputBox");
-var button = $("button"); 
-	button.addEventListener ("click", function() {
+	var $inputBox = $("#inputBox");
+	var $button = $("#button"); 
+	var $savedStuff = $("#savedStuff");	
+	var $list = $("#list");
+    var $list = [];
+
+	$button.addEventListener ("click", function() {
 		console.log(inputBox.value);
-});
+	});
 
-var myArray = []; //create empty array
-	button.addEventListener ("click", function() { //listen for button click, when clicked run function
-		myArray.push(inputBox.value); // push the value in the inputBox into the array
-		console.log(myArray); //log array values
-});
+	$savedStuff.on("submit", addStuff);
 
 
-var inputBox = $("inputBox");
-var todoList = $("savedStuff");
+		function addStuff(e) {
 
-input.addEventListener("keyup", appendMessage);
+			e.preventDefault();
+			if(event.keyCode === 13) { 
+			savedStuff.innerHTML += "<div>" + inputBox.value + "</div>";
+			inputBox.value("");
+			}
 
-function appendMessage() {
-	if(event.keyCode === 13) { //can't figure out how to make this happen on button click, too
-		
-	savedStuff.innerHTML += "<div>" + input.value + "</div>";
-	input.value = "";
-	}
-};
+			listStuff = render(list);
+			$list.html(listStuff);
+		}	
+			
+	    $list.on("click", "li", strike);
 
-var newStuff = [];
-var messageBox  = document.getElementById("savedStuff");
+	    function strike() {
+	        $(this).toggleClass("strikethrough");
+	    }
 
-function render(todoList) {
+		function render(todoList) {
 		return '<ul><li class = "todo">'+todoList.join('</li><li>')+'</li></ul>';
 	}
-
-function strikethrough(event) {
-	var target= $(event.target);
-	if (target.is("li")) {
-		target.style.css('text-decoration', 'underline');
 }
 
-
-// function strikethrough(e) {
-// 		var i = e.target;
-// 		// through(i);
-// 		(e.target).style.textDecoration = "line-through";
-// 	}
 
 
 //my attempt at "hard mode"
